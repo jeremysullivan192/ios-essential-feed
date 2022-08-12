abd3e27c7b34290841cf863c02240a760cd96127
Jeremy Sullivan <jsullivan@amientertainment.com>
2022-08-12 18:31:11 -0500

    RemoteFeedLoader gets HTTPClient constructor injected

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,16 +9,44 @@ import Foundation
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load(){
+        client.get(from: URL(string: "url.com")!)
+    }
 }
 
-class HTTPClient {
-    var requestedURL: URL?
+protocol HTTPClient {
+    func get(from: URL)
 }
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+    
+    func get(from: URL) {
+        requestedURL = from
+    }
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
                 
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromUrl() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+        
     }
 }
