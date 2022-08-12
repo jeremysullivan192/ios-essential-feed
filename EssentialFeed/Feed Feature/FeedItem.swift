08d7d85aaa0e69291b6fedd0d01f8eee9e787a87
Jeremy Sullivan <jsullivan@amientertainment.com>
2022-08-12 14:12:15 -0500

    first commit, create feed item and feed loader

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by jsullivan on 8/8/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
