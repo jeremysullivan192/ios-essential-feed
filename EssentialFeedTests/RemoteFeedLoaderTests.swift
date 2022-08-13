c7ed605a7fd33d69a716cf129fb967ac3d4161c8
Jeremy Sullivan <jsullivan@amientertainment.com>
2022-08-12 19:13:57 -0500

    added .gitignore, made test helper methods

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -26,30 +26,33 @@ protocol HTTPClient {
     func get(from: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from: URL) {
-        requestedURL = from
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: URL(string: "test.com")!, client: client)
+        let (_, client) = makeSUT()
                 
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromUrl() {
         let url = URL(string: "loadit.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL = URL(string: "test.com")!) -> (sut: RemoteFeedLoader, client:HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
         
+        func get(from: URL) {
+            requestedURL = from
+        }
     }
 }
